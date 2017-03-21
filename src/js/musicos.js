9a673d7ae590082e245fd36adb43e01506252553
Michael Bergen <webtechmike@gmail.com>
2017-03-20 20:37:02 -0700

    add some basic tests

--- src/js/musicos.js
+++ src/js/musicos.js
@@ -1,16 +1,23 @@
 class Character {
-    constructor(name, bands, instrument, yrsExperience, axes, outfit) {
+    constructor(name, instruments, defaultStyle, defaultRiff, preferredKey, influences, axes, yrsExperience, homeTown, bands, outfit) {
         this.name = name;
-        this.bands = bands;
-        this.instrument = instrument;
-        this.yrsExperience = yrsExperience;
+        this.instruments = instruments;
+        this.defaultStyle = defaultStyle;
+        this.defaultRiff = defaultRiff;
+        this.preferredKey = preferredKey;
+        this.influences = influences;
         this.axes = axes;
+        this.yrsExperience = yrsExperience;
+        this.homeTown = homeTown;
+        this.bands = bands;
         this.outfit = outfit;
     }
 }
 
-var mike = new Character('Michael Bergen', ['Fire Burn', 'Dirty Science Crew'], ['piano', 'keyboards', 'drums'], 18, ['Yamaha Clavinova', 'Korg Triton Extreme', 'Roland Juno-D', 'Break Beats Kit'], 'Jeans & T-Shirt');
+var mike = new Character('Michael Bergen', ['piano', 'keyboards', 'drums'], 'roots', 'someFile.mp3', 'Eb', ['Jackie Mitoo', 'Bill Evans', 'Thelonius Monk', 'Bob Marley', 'Duke Ellington', 'Charlie Parker', 'Miles Davis'], ['Korg Triton Extreme', 'Roland Juno-D', 'Break Beats Kit'], 18, 'Aliso Viejo', ['Fire Burn', 'Dirty Science Crew'], 'Jeans & T-Shirt');
 
-var musicos = [mike];
+var tony = new Character('Tony Browning', ['drums'], ['downtempo', 'rock'], 'someOtherFile.mp3', 'N/A', ['The Cure', 'Depeche Mode'], ['Ludwig Kit'], 20, 'Laguna Hills', ['Red Lights', 'Dirty Science Crew'], 'Jeans & T-Shirt');
+
+var musicos = [mike, tony];
 
 console.log(musicos);
--- test/musicos.test.js
+++ test/musicos.test.js
@@ -1,5 +1,11 @@
-var assert = chai.assert;
-var expect = chai.expect;
+const assert = chai.assert;
+const expect = chai.expect;
+
+const min = Math.floor(0);
+const max = Math.floor(musicos.length - 1);
+const randomIndex = parseInt(Math.random() * (max - min + 1) + min);
+
+console.log(min, max, randomIndex);
 
 describe('Musicos', () => {
     it('should run mocha tests', () => {
@@ -8,10 +14,31 @@ describe('Musicos', () => {
     it('should log a string to the console', () => {
         expect(musicos).to.be.an('array');
     });
+    it('should start out with Mike', () => {
+        expect(musicos[0]).to.be.an('object');
+    });
 });
 
 describe('Characters', () => {
-    it('should start out with Mike', () => {
-        expect(musicos[0]).to.be.an('object');
+    it('should have a default instruments array', () => {
+        expect(musicos[randomIndex].instruments).to.exist;
+    });
+    it('should have a default style', () => {
+        expect(musicos[randomIndex].defaultStyle).to.exist;
+    });
+    it('should have a default riff', () => {
+        expect(musicos[randomIndex].defaultRiff).to.exist;
+    });
+    it('should have a default outfit', () => {
+        expect(musicos[randomIndex].outfit).to.exist;
+    });
+    it('should have a preferred key', () => {
+        expect(musicos[randomIndex].preferredKey).to.exist;
+    });
+    it('should have a list of 3 influences', () => {
+        expect(musicos[randomIndex].influences).to.exist;
+    });
+    it('should have a hometown', () => {
+        expect(musicos[randomIndex].homeTown).to.exist;
     });
 });
