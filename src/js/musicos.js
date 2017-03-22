c5156bd744e597e09e5d628468b4bc8524098a6a
Michael Bergen <webtechmike@gmail.com>
2017-03-21 21:52:00 -0700

    add band and show classes

--- src/js/musicos.js
+++ src/js/musicos.js
@@ -14,10 +14,35 @@ class Character {
     }
 }
 
-var mike = new Character('Michael Bergen', ['piano', 'keyboards', 'drums'], 'roots', 'someFile.mp3', 'Eb', ['Jackie Mitoo', 'Bill Evans', 'Thelonius Monk', 'Bob Marley', 'Duke Ellington', 'Charlie Parker', 'Miles Davis'], ['Korg Triton Extreme', 'Roland Juno-D', 'Break Beats Kit'], 18, 'Aliso Viejo', ['Fire Burn', 'Dirty Science Crew'], 'Jeans & T-Shirt');
+class Band {
+    constructor(name, members, tourDates, homeTown, genre) {
+        this.name = name;
+        this.members = members;
+        this.tourDates = tourDates;
+        this.homeTown = homeTown;
+        this.genre = genre;
+    }
+}
 
-var tony = new Character('Tony Browning', ['drums'], ['downtempo', 'rock'], 'someOtherFile.mp3', 'N/A', ['The Cure', 'Depeche Mode'], ['Ludwig Kit'], 20, 'Laguna Hills', ['Red Lights', 'Dirty Science Crew'], 'Jeans & T-Shirt');
+class Show {
+    constructor(name, talent, location, date){
+        this.name = name;
+        this.talent = talent;
+        this.location = location;
+        this.date = date;
+    }
+}
 
-var musicos = [mike, tony];
+let d = new Date();
 
-console.log(musicos);
+let mike = new Character('Michael Bergen', ['piano', 'keyboards', 'drums'], 'roots', 'someFile.mp3', 'Eb', ['Jackie Mitoo', 'Bill Evans', 'Thelonius Monk', 'Bob Marley', 'Duke Ellington', 'Charlie Parker', 'Miles Davis'], ['Korg Triton Extreme', 'Roland Juno-D', 'Break Beats Kit'], 18, 'Aliso Viejo', ['Fire Burn', 'Dirty Science Crew'], 'Jeans & T-Shirt');
+
+let tony = new Character('Tony Browning', ['drums'], ['downtempo', 'rock'], 'someOtherFile.mp3', 'N/A', ['The Cure', 'Depeche Mode'], ['Ludwig Kit'], 20, 'Laguna Hills', ['Red Lights', 'Dirty Science Crew'], 'Jeans & T-Shirt');
+
+let dsc = new Band('DSC', [mike, tony], [d], ['Laguna Hills'], ['Hip Hop'])
+
+let musicos = [mike, tony];
+let bands = [dsc];
+let sxsw = new Show('Lalapalooza', [dsc], 'Texas', d);
+
+console.log(musicos, bands, sxsw);
